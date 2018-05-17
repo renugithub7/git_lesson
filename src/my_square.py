88c898df2cee724049acafa418035b4e5c6dad34
Renuka Raghuraman <renuka788@gmail.com>
2018-05-18 00:00:53 +1000

    x -> y and square 42

--- src/my_square.py
+++ src/my_square.py
@@ -1,6 +1,6 @@
-def my_square(x):
+def my_square(y):
 	"""takes a value and returns the squared value
 	uses the ** operator
 	"""
-	return(x ** 2)
-print(my_square(4))
+	return(y ** 2)
+print(my_square(42))
